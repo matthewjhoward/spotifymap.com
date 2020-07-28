0f5176de8fb749fa65af582247a19abcec4cef84
Matt <matthoward@ucsc.edu>
2020-07-28 10:40:50 -0700

    Modified broken uuid import

--- src/components/Widget/Widget.js
+++ src/components/Widget/Widget.js
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types';
 import { UncontrolledTooltip } from 'reactstrap';
 import s from './Widget.module.scss';
 import classNames from 'classnames';
-import Loader from '../Loader'; // eslint-disable-line css-modules/no-unused-class
+import Loader from '../Loader'; 
 import AnimateHeight from 'react-animate-height';
-import uuidv4 from 'uuid/v4'
+import {v4 as uuidv4} from 'uuid'
+
 import {
   DropdownToggle,
   DropdownMenu,
