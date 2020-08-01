12300ce9972df53367941fae5f017881d6b45275
Matt <matthoward@ucsc.edu>
2020-08-01 12:37:54 -0700

    Move copy operation to bash script

--- cron-run.sh
+++ cron-run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 echo $(date) >> /var/www/spotifymap.com/bash_cron_log.txt
 /usr/bin/python3 /var/www/spotifymap.com/python/scraper.py >> /var/www/spotifymap.com/bash_cron_log.txt 2>&1
+cp -a /var/www/spotifymap.com/python/data/exports/. /var/www/spotifymap.com/src/data/
