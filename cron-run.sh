0e702ebcdbbe575c344dfd8802b208b2692ef90f
Matt <matthoward@ucsc.edu>
2020-08-01 09:28:36 -0700

    bash run updated

--- cron-run.sh
+++ cron-run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo $(date) >> /bash_cron_log.txt
-/usr/bin/python3 /var/www/spotifymap.com/python/scraper.py >> /bash_cron_log.txt
+echo $(date) >> ~/bash_cron_log.txt
+/usr/bin/python3 /var/www/spotifymap.com/python/scraper.py >> ~/bash_cron_log.txt
