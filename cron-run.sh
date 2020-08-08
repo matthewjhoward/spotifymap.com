4fdd296a82789ac3c6139d1c9d27188731b15a9e
Matt <matthoward@ucsc.edu>
2020-08-08 12:28:33 -0700

    modified cron to get proper working directory

--- cron-run.sh
+++ cron-run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-echo $(date) >> /var/www/spotifymap.com/bash_cron_log.txt
-/usr/bin/python3 /var/www/spotifymap.com/python/scraper.py >> /var/www/spotifymap.com/bash_cron_log.txt 2>&1
-cp -a /var/www/spotifymap.com/python/data/exports/. /var/www/spotifymap.com/src/data/
+cd /var/www/spotifymap.com/python 
+echo $(date) > bash_cron_log.txt
+/usr/bin/python3 scraper.py > bash_cron_log.txt 2>&1
+cp -a data/exports/. /var/www/spotifymap.com/src/data/
